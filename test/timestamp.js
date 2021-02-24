0577da50e4730f29aa19492120dda472e91834b9
Jonas Waldén (Exp) <jonas.walden@expressen.se>
2021-02-24 13:28:08 +0100

    fixed timestamp rounding errors and tests for new format

--- test/timestamp.js
+++ test/timestamp.js
@@ -6,16 +6,16 @@ const makeTimestamp = require("../lib/timestamp");
 describe("timestamp", () => {
   it("short duration in milliseconds", () => {
     const timestamp = makeTimestamp(789);
-    assert.equal(timestamp, "789 ms");
+    assert.deepEqual(timestamp, ["789", "ms"]);
   });
 
   it("medium duration in seconds", () => {
     const timestamp = makeTimestamp(35789);
-    assert.equal(timestamp, "35.8 s");
+    assert.deepEqual(timestamp, ["35.8", "s"]);
   });
 
   it("long duration in minutes and seconds", () => {
     const timestamp = makeTimestamp(75789);
-    assert.equal(timestamp, "1 m 15 s");
+    assert.deepEqual(timestamp, ["1 m 16", "s"]);
   });
 });
